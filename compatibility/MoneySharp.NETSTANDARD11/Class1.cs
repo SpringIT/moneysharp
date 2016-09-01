ea2e3c89d1050d23f9b04e2a5a11e557e6278c05
Maarten Mensink <maarten.mensink@spring-it.nl>
2016-09-01 11:33:10 +0200

    setup netstandard projects and compatibility projects

---
+++ compatibility/MoneySharp.NETSTANDARD11/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MoneySharp.NETSTANDARD11
+{
+    public class Class1
+    {
+        public Class1()
+        {
+        }
+    }
+}
