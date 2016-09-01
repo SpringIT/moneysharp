3a68b29f95d62aa1a5da9d99b5d5152886149000
Maarten Mensink <maarten.mensink@spring-it.nl>
2016-09-01 13:07:51 +0200

    clean compatibility

--- compatibility/MoneySharp.NETSTANDARD11/Class1.cs
+++ compatibility/MoneySharp.NETSTANDARD11/Class1.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using RestSharp;
-
-namespace MoneySharp.NETSTANDARD11
+﻿namespace MoneySharp.NETSTANDARD11
 {
     public class Class1
     {
